df1bb72320351667b1030dde1f27518341ee1b68
Jason Vargo <jvargo@gmail.com>
2014-09-03 10:48:45 -0500

    corrected plots

--- melt_NWS_data.R
+++ melt_NWS_data.R
@@ -17,7 +17,7 @@ unique(allStationsClean[,"County"])
 #---------------Check agreement of stations and NWS-------------------------------
 
 stationsNWS <- merge(allStationsClean, allAdvisories, by=c("Day", "Month", "Year", "County"), all.x=TRUE)
-stationsNWS2011On <- stationsNWS[which(stationsNWS$Year > 2011 & stationsNWS$Year < 2013),]
+stationsNWS2011On <- stationsNWS[which(stationsNWS$Year > 2008 & stationsNWS$Year < 2013),]
 
 
 stationsNWS2011OnClean <- na.omit(stationsNWS2011On)
@@ -39,7 +39,7 @@ stationsNWS2011OnClean[falseneg,"test"] <-"falsenegative"
 
 #imperv <- read.csv("~/Box Sync/work/MODISsummer/ATL_imperv_GIS/station_imerv_results.csv", header=T) 
 
-stationTestCounts <- dcast(stationsNWS2011OnClean, County + Station ~test, length)
+stationTestCounts <- dcast(stationsNWS2011OnClean, County + Station + Metro.x + Long + Lat ~test, length)
 stationTestCounts$sensitivity <- stationTestCounts$truepos /(stationTestCounts$truepos+stationTestCounts$falseneg)
 stationTestCounts$specificity <- stationTestCounts$trueneg /(stationTestCounts$trueneg+stationTestCounts$falsepos)
 stationTestCounts$obs <- stationTestCounts$truepos + stationTestCounts$falsepos + stationTestCounts$trueneg + stationTestCounts$falseneg
@@ -47,23 +47,24 @@ stationTestCounts$obs <- stationTestCounts$truepos + stationTestCounts$falsepos 
 #stationTestCounts <- merge(stationTestCounts, imperv, by="Station", all.x=TRUE, all.y=FALSE)
 #write.csv(stationTestCounts, "~/Box Sync/work/MODISsummer/paper/analysis/stationTestCounts.csv", row.names=FALSE, na="")
 
-ggplot(stationTestCounts, aes(x=X500m, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+#ggplot(stationTestCounts, aes(x=X500m, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
 
 
-#------------------------percent developed---------------------------------------
-ggplot(stationTestCounts, aes(x=pct_developed, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# #------------------------percent developed---------------------------------------
+# ggplot(stationTestCounts, aes(x=pct_developed, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# quartz()
+# ggplot(stationTestCounts, aes(x=pct_developed, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# 
+# 
+# quartz()
+# ggplot(stationTestCounts, aes(x=Long, y=Lat, size=obs, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and % developed") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# 
 quartz()
-ggplot(stationTestCounts, aes(x=pct_developed, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+ggplot(stationTestCounts, aes(x=Long, y=Lat, size=2, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and sensitivity 2012") + theme(legend.position="bottom") + facet_grid(Metro.x ~ ., scales="free")
+ggplot(stationTestCounts, aes(x=Long, y=Lat, size=2, color=specificity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and sensitivity 2012") + theme(legend.position="bottom") + facet_grid(Metro.x ~ ., scales="free")
 
 
-quartz()
-ggplot(stationsNWS2011OnClean, aes(x=Long, y=Lat, size=obs, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and % developed") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
-
-quartz()
-ggplot(stationsNWS2011OnClean, aes(x=Long, y=Lat, size=2, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and sensitivity 2012") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
-
-
-ggplot(stationsNWS2011OnClean, aes(x=X500m, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Specificity") + ggtitle("Station Specificity (Station = gold standard)") + theme(legend.position="bottom")#+ facet_grid(.~Year)
+ggplot(stationTestCounts, aes(x=sensitivity, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("Sensitivity") + ylab("Specificity") + ggtitle("NWS Accuracy 2008-2012 (Station = gold standard)") + theme(legend.position="bottom")+ facet_grid(. ~ Metro.x)
 
 
 #-----------------------------------------------
