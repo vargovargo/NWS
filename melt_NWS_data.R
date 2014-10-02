15b0786a8ebf85dfc0f87442b50b6a34306254bf
vargovargo <jvargo+git@gmail.com>
2014-10-02 14:42:49 -0500

    initial look at LST data

--- melt_NWS_data.R
+++ melt_NWS_data.R
@@ -39,12 +39,95 @@ p + geom_point(aes(x=date, y=max_RH, color=factor(NWSadvisory)),stat="identity")
 
 p
 #write.csv(unique(read.csv("./stations_4_unique.csv", header=T)), "./uniqStations.csv")
+
+
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
 #---------------Check agreement of stations and NWS-------------------------------
 
 stationsNWS <- merge(allStationsClean, allAdvisories, by=c("Day", "Month", "Year", "County"), all.x=TRUE)
 stationsNWS2011On <- stationsNWS[which(stationsNWS$Year > 2005 & stationsNWS$Year < 2013),]
 
 stationsNWS2011OnClean <- na.omit(stationsNWS2011On)
+
+truepos <- which(stationsNWS2011OnClean$NWSadvisory == 1 & stationsNWS2011OnClean$Heat_Day == 1) 
+falseneg <- which(stationsNWS2011OnClean$NWSadvisory == 0 & stationsNWS2011OnClean$Heat_Day == 1) 
+trueneg <- which(stationsNWS2011OnClean$NWSadvisory == 0 & stationsNWS2011OnClean$Heat_Day == 0) 
+falsepos <- which(stationsNWS2011OnClean$NWSadvisory == 1 & stationsNWS2011OnClean$Heat_Day == 0) 
+
+stationsNWS2011OnClean$test <- ""
+
+stationsNWS2011OnClean[truepos,"test"] <-"truepositive"
+stationsNWS2011OnClean[trueneg,"test"] <-"truenegative"
+stationsNWS2011OnClean[falsepos,"test"] <-"falsepositive"
+stationsNWS2011OnClean[falseneg,"test"] <-"falsenegative"
+ 
+
+#---------------Calculate Sensitivity/Specificity--------------------------------
+
+#imperv <- read.csv("~/Box Sync/work/MODISsummer/ATL_imperv_GIS/station_imerv_results.csv", header=T) 
+
+stationTestCounts <- dcast(stationsNWS2011OnClean, County + Station + Metro.x + Long + Lat ~test, length)
+stationTestCounts$sensitivity <- stationTestCounts$truepos /(stationTestCounts$truepos+stationTestCounts$falseneg)
+stationTestCounts$specificity <- stationTestCounts$trueneg /(stationTestCounts$trueneg+stationTestCounts$falsepos)
+stationTestCounts$obs <- stationTestCounts$truepos + stationTestCounts$falsepos + stationTestCounts$trueneg + stationTestCounts$falseneg
+
+#stationTestCounts <- merge(stationTestCounts, imperv, by="Station", all.x=TRUE, all.y=FALSE)
+#write.csv(stationTestCounts, "~/Box Sync/work/MODISsummer/paper/analysis/stationTestCounts.csv", row.names=FALSE, na="")
+
+#ggplot(stationTestCounts, aes(x=X500m, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+
+
+# #------------------------percent developed---------------------------------------
+# ggplot(stationTestCounts, aes(x=pct_developed, y=sensitivity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# quartz()
+# ggplot(stationTestCounts, aes(x=pct_developed, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("% impervious") + ylab("Sensitivity") + ggtitle("Station Sensitivity (Station = gold standard)") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# 
+# 
+# quartz()
+# ggplot(stationTestCounts, aes(x=Long, y=Lat, size=obs, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Station observations and % developed") + theme(legend.position="bottom")# + geom_smooth(method = "lm", se=TRUE)# + facet_grid(.~County)
+# 
+quartz()
+par(mfrow=c(2,2))
+ggplot(subset(stationTestCounts, Metro.x == "Atlanta"), aes(x=Long, y=Lat, size=obs, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Atlanta station observations and sensitivity 2008-2012") + theme(legend.position="bottom")
+ggplot(subset(stationTestCounts, Metro.x == "Atlanta"), aes(x=Long, y=Lat, size=obs, color=specificity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Atlanta station observations and specificity 2008-2012") + theme(legend.position="bottom") 
+ggplot(subset(stationTestCounts, Metro.x == "Chicago"), aes(x=Long, y=Lat, size=obs, color=sensitivity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Chicago station observations and sensitivity 2008-2012") + theme(legend.position="bottom")
+ggplot(subset(stationTestCounts, Metro.x == "Chicago"), aes(x=Long, y=Lat, size=obs, color=specificity)) + geom_point(stat="identity") + xlab("Long") + ylab("Lat") + ggtitle("Chicago station observations and specificity 2008-2012") + theme(legend.position="bottom") 
+
+
+ggplot(stationTestCounts, aes(x=sensitivity, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("Sensitivity") + ylab("Specificity") + ggtitle("NWS Accuracy 2008-2012 (Station = gold standard)") + theme(legend.position="bottom")+ facet_grid(. ~ Metro.x)
+
+write.csv(stationTestCounts, "./stationTestsForGIS.csv", row.names=FALSE, na="")
+
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+#---------------Check agreement of LST and NWS-------------------------------
+allStationsLST <- read.csv("./LST_Station.csv", header=T)
+
+stationsLSTNWS <- merge(allStationsLST, allAdvisories, by=c("Day", "Month", "Year", "County"), all.x=TRUE)
+stationsNWS2011On <- stationsLSTNWS[which(stationsLSTNWS$Year > 2005 & stationsLSTNWS$Year < 2013),]
+stationsNWS2011OnClean <- na.omit(stationsNWS2011On)
+
+ggplot(stationsNWS2011OnClean, aes(x=factor(Month), y=Temp,fill=factor(NWSadvisory))) + geom_boxplot() + labs(x="Month", y="LST at Station (˚F)") + facet_grid(County ~ Year) + theme(legend.position="bottom")
+
+allCountiesLST <- read.csv("./LST_County.csv", header=T)
+
+countiesLSTNWS <- merge(allCountiesLST, allAdvisories, by=c("Day", "Month", "Year", "County"), all.x=TRUE)
+countiesNWS2011On <- countiesLSTNWS[which(countiesLSTNWS$Year > 2005 & countiesLSTNWS$Year < 2013),]
+countiesNWS2011OnClean <- na.omit(countiesNWS2011On)
+
+ggplot(countiesNWS2011OnClean, aes(x=factor(Month), y=Max,fill=factor(NWSadvisory))) + geom_boxplot() + labs(x="Month", y="LST County Max (˚F)") + facet_grid(County ~ Year) + theme(legend.position="bottom")
+
+
+
+
 
 truepos <- which(stationsNWS2011OnClean$NWSadvisory == 1 & stationsNWS2011OnClean$Heat_Day == 1) 
 falseneg <- which(stationsNWS2011OnClean$NWSadvisory == 0 & stationsNWS2011OnClean$Heat_Day == 1) 
@@ -94,6 +177,32 @@ ggplot(subset(stationTestCounts, Metro.x == "Chicago"), aes(x=Long, y=Lat, size=
 ggplot(stationTestCounts, aes(x=sensitivity, y=specificity, color=factor(County), size=obs)) + geom_point(stat="identity") + xlab("Sensitivity") + ylab("Specificity") + ggtitle("NWS Accuracy 2008-2012 (Station = gold standard)") + theme(legend.position="bottom")+ facet_grid(. ~ Metro.x)
 
 write.csv(stationTestCounts, "./stationTestsForGIS.csv", row.names=FALSE, na="")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #----------------calculate County stats-------------------------------
 
